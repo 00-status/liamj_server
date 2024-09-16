80aa0fc9d699bb380ff7fd6537eb43c982c9ac1d
00-status <dedypips@gmail.com>
2024-09-16 12:34:42 -0600

    Randomly adding extra damage.

--- Lib/WeaponGenerator/Application/GenerateWeaponService.cs
+++ Lib/WeaponGenerator/Application/GenerateWeaponService.cs
@@ -1,9 +1,11 @@
 public class GenerateWeaponService
 {
+    private readonly MundaneWeaponContext MundaneWeaponContext;
     private readonly WeaponEffectDBContext WeaponEffectDBContext;
 
     public GenerateWeaponService(WeaponEffectDBContext weaponEffectDBContext)
     {
+        MundaneWeaponContext = new();
         this.WeaponEffectDBContext = weaponEffectDBContext;
     }
 
@@ -13,10 +15,14 @@ public class GenerateWeaponService
 
         MundaneWeapon randomMundaneWeapon = PickMundaneWeapon();
 
+        Random random = new();
+        int randomIndex = random.Next(MundaneWeaponContext.ExtraDamages.Count());
+        ExtraDamage extraDamage = MundaneWeaponContext.ExtraDamages[randomIndex];
+
         Weapon generatedWeapon = builder
             .SetRarity("Uncommon")
             .SetMundaneWeaponProperties(randomMundaneWeapon)
-            .SetExtraDamage(new WeaponDamage(1, 6, "Slashing"))
+            .SetExtraDamage(extraDamage.WeaponDamage)
             .Build();
 
         return Results.Ok(generatedWeapon);
@@ -24,20 +30,20 @@ public class GenerateWeaponService
         // Instantiates a new builder. ✅
         // Randomly picks a base weapon. ✅
         // Adds base weapon properties to builder. ✅
-        // randomly picks extra damage based on rarity.
+        // randomly picks extra damage. ✅
         // Fetches WeaponEffects from the DB.
-        // randomly picks a WeaponEffect based on rarity.
+        // randomly picks a WeaponEffect.
         // Build the new weapon. ✅
         // return the new weapon through the API. ✅
+        // Select traits on each step based on rarity.
     }
 
-    private static MundaneWeapon PickMundaneWeapon()
+    private MundaneWeapon PickMundaneWeapon()
     {
-        MundaneWeaponContext mundaneWeaponContext = new();
         Random random = new();
 
-        int randomIndex = random.Next(mundaneWeaponContext.MundaneWeapons.Count);
+        int randomIndex = random.Next(MundaneWeaponContext.MundaneWeapons.Count);
 
-        return mundaneWeaponContext.MundaneWeapons[randomIndex];
+        return MundaneWeaponContext.MundaneWeapons[randomIndex];
     }
 }

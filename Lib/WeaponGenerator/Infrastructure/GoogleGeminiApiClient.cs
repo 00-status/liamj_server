b43072bb7d7159ba05c5c86553bd8e206e68176e
00-status <dedypips@gmail.com>
2024-09-18 11:53:11 -0600

    Fixed type error

--- Lib/WeaponGenerator/Infrastructure/GoogleGeminiApiClient.cs
+++ Lib/WeaponGenerator/Infrastructure/GoogleGeminiApiClient.cs
@@ -45,23 +45,23 @@ public class GoogleGeminiApiClient
 public class Root
 {
     [JsonPropertyName("candidates")]
-    public List<Candidate> Candidates { get; set; }
+    public required List<Candidate> Candidates { get; set; }
 }
 
 public class Candidate
 {
     [JsonPropertyName("content")]
-    public Content Content { get; set; }
+    public required Content Content { get; set; }
 }
 
 public class Content
 {
     [JsonPropertyName("parts")]
-    public List<Part> Parts { get; set; }
+    public required List<Part> Parts { get; set; }
 }
 
 public class Part
 {
     [JsonPropertyName("text")]
-    public string Text { get; set; }
+    public required string Text { get; set; }
 }
